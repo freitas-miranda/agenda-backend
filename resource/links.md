719d0f37f6b8dddacbc27ea61b79faae8722e085
Alan Miranda <freitas.miranda@gmail.com>
2023-07-28 15:21:32 -0400

    feat: adiciona typeorm

---
+++ resource/links.md
@@ -0,0 +1,21 @@
+# Lista dos materias de apoio para o projeto
+
+## NestJS
+https://docs.nestjs.com/
+
+## TypeORM
+https://docs.nestjs.com/techniques/database
+
+## Soft Delete
+https://stackoverflow.com/questions/73189707/how-to-do-prisma-soft-delete-with-nestjs
+https://www.youtube.com/watch?v=EKtrqq9eeyk&list=PLiPDj1LrYXr0M1gXepUacs9FvL4dJU40q
+
+## DTO
+https://www.linkedin.com/pulse/melhores-pr%C3%A1ticas-do-dto-data-transfer-object-com-nestjs-raposo/?originalSubdomain=pt
+
+## Swagger
+https://www.youtube.com/watch?v=PfSyDd3Sc5A&list=PLiPDj1LrYXr0M1gXepUacs9FvL4dJU40q&index=2
+
+
+## Autenticação
+https://www.youtube.com/watch?v=jMprSQlDLGo
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', 'sql@dev'),
+        database: configService.get('DB_DATABASE', 'marcaja'),
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      }),
+    }),
+  ],
+})
+export class AppModule {}
