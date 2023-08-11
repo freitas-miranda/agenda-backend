19c91dcfc2a456d41c0db3724d3a84b89b44894b
Alan Miranda <freitas.miranda@gmail.com>
2023-08-11 13:32:52 +0000

    chore: adiciona dev container

--- src/configs/env/env.module.ts
+++ src/configs/env/env.module.ts
@@ -6,7 +6,7 @@ import { validationSchema } from './validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `src/configs/env/.env.${process.env.NODE_ENV}`,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
       load: [configuration],
       validationSchema,
