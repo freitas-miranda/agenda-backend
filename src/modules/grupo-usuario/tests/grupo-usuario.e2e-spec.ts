3b0599d547660ba4ae97ac28b8787469fb131000
Alan Miranda <freitas.miranda@gmail.com>
2023-08-15 16:17:29 +0000

    test: adiciona teste e2e para relacionar permissao com grupo

--- src/modules/grupo-usuario/tests/grupo-usuario.e2e-spec.ts
+++ src/modules/grupo-usuario/tests/grupo-usuario.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { AppModule } from '@src/app.module';
 
-const params = {
+const paramsGrupo = {
   descricao: 'Administradores',
 };
 
@@ -13,10 +13,16 @@ const paramsUsuario = {
   senha: '12345678',
 };
 
+const paramsPermissao = {
+  key: 'permisao_teste_grupo_usuario',
+  descricao: 'Permissão para testar o relacionamento',
+};
+
 describe('GrupoUsuario (e2e)', () => {
   let app: INestApplication;
   let access_token: string;
   let usuario: any;
+  let permissao: any;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -42,12 +48,25 @@ describe('GrupoUsuario (e2e)', () => {
     }
 
     // Obter uma permissão válida
+    const permissoes = await request(app.getHttpServer())
+      .get('/api/v1/permissao?key=' + paramsPermissao.key)
+      .set('Authorization', 'Bearer ' + access_token);
+    if (permissoes.body.length > 0) {
+      permissao = permissoes.body[0];
+    } else {
+      const retorno = await request(app.getHttpServer())
+        .post('/api/v1/permissao')
+        .set('Authorization', 'Bearer ' + access_token)
+        .send(paramsPermissao)
+        .expect(HttpStatus.CREATED);
+      permissao = retorno.body;
+    }
   });
 
   it('[POST /api/v1/grupo-usuario] Criar um grupo de usuário', async () => {
     // Localizar grupo usuário pela descrição
     const registros = await request(app.getHttpServer())
-      .get('/api/v1/grupo-usuario?descricao=' + params.descricao)
+      .get('/api/v1/grupo-usuario?descricao=' + paramsGrupo.descricao)
       .set('Authorization', 'Bearer ' + access_token);
 
     // Remover o grupo de usuário de teste caso exista
@@ -62,26 +81,26 @@ describe('GrupoUsuario (e2e)', () => {
     const retorno = await request(app.getHttpServer())
       .post('/api/v1/grupo-usuario')
       .set('Authorization', 'Bearer ' + access_token)
-      .send(params)
+      .send(paramsGrupo)
       .expect(HttpStatus.CREATED);
 
     expect(retorno.body).toHaveProperty('id');
     expect(retorno.body.id).toBeTruthy();
-    params['id'] = retorno.body.id;
+    paramsGrupo['id'] = retorno.body.id;
 
     expect(retorno.body).not.toHaveProperty('descricao');
   });
 
   it('[GET /api/v1/grupo-usuario/:id] Exibir um grupo de usuário', async () => {
     const retorno = await request(app.getHttpServer())
-      .get('/api/v1/grupo-usuario/' + params['id'])
+      .get('/api/v1/grupo-usuario/' + paramsGrupo['id'])
       .set('Authorization', 'Bearer ' + access_token)
       .expect(HttpStatus.OK);
 
     const registro = retorno.body;
     expect(registro).toBeDefined();
-    expect(registro.id).toEqual(params['id']);
-    expect(registro.descricao).toEqual(params.descricao);
+    expect(registro.id).toEqual(paramsGrupo['id']);
+    expect(registro.descricao).toEqual(paramsGrupo.descricao);
   });
 
   it('[GET /api/v1/grupo-usuario] Listar todos grupos de usuários', async () => {
@@ -121,7 +140,7 @@ describe('GrupoUsuario (e2e)', () => {
     const novaDescricao = 'Nova descrição do grupo de usuários';
 
     await request(app.getHttpServer())
-      .patch('/api/v1/grupo-usuario/' + params['id'])
+      .patch('/api/v1/grupo-usuario/' + paramsGrupo['id'])
       .set('Authorization', 'Bearer ' + access_token)
       .send({ descricao: novaDescricao })
       .expect(HttpStatus.OK)
@@ -130,7 +149,7 @@ describe('GrupoUsuario (e2e)', () => {
       });
 
     return request(app.getHttpServer())
-      .get('/api/v1/grupo-usuario/' + params['id'])
+      .get('/api/v1/grupo-usuario/' + paramsGrupo['id'])
       .set('Authorization', 'Bearer ' + access_token)
       .expect(HttpStatus.OK)
       .then((value) => {
@@ -140,7 +159,7 @@ describe('GrupoUsuario (e2e)', () => {
 
   it('[DELETE /api/v1/grupo-usuario/:id] Deletar um grupo de usuário', async () => {
     return request(app.getHttpServer())
-      .delete('/api/v1/grupo-usuario/' + params['id'])
+      .delete('/api/v1/grupo-usuario/' + paramsGrupo['id'])
       .set('Authorization', 'Bearer ' + access_token)
       .expect(HttpStatus.OK)
       .then((value) => {
@@ -150,7 +169,7 @@ describe('GrupoUsuario (e2e)', () => {
 
   it('[POST /api/v1/grupo-usuario/:id/usuario/:usuarioId] Adicionar um usuário no grupo', async () => {
     await request(app.getHttpServer())
-      .post('/api/v1/grupo-usuario/' + params['id'] + '/usuario/' + usuario.id)
+      .post('/api/v1/grupo-usuario/' + paramsGrupo['id'] + '/usuario/' + usuario.id)
       .set('Authorization', 'Bearer ' + access_token)
       .expect(HttpStatus.CREATED)
       .then((retorno) => {
@@ -162,11 +181,33 @@ describe('GrupoUsuario (e2e)', () => {
 
   it('[DELETE /api/v1/grupo-usuario/:id/usuario/:usuarioId] Adicionar um usuário no grupo', async () => {
     await request(app.getHttpServer())
-      .delete('/api/v1/grupo-usuario/' + params['id'] + '/usuario/' + usuario.id)
+      .delete('/api/v1/grupo-usuario/' + paramsGrupo['id'] + '/usuario/' + usuario.id)
       .set('Authorization', 'Bearer ' + access_token)
       .expect(HttpStatus.OK)
       .then((retorno) => {
-        expect(retorno.body).toEqual({ mensagem: 'Usuário removido do grupo!' });
+        expect(retorno.body.mensagem).toContain('Usuário removido do grupo!');
+      });
+  });
+
+  it('[POST /api/v1/grupo-usuario/:id/permissao/:permissaoId] Adicionar uma permissão ao grupo', async () => {
+    await request(app.getHttpServer())
+      .post('/api/v1/grupo-usuario/' + paramsGrupo['id'] + '/permissao/' + permissao.id)
+      .set('Authorization', 'Bearer ' + access_token)
+      .expect(HttpStatus.CREATED)
+      .then((retorno) => {
+        expect(retorno.body).toHaveProperty('id');
+        expect(retorno.body.mensagem).toContain('Permissão adicionada ao grupo!');
+        expect(retorno.body.id).toBeTruthy();
+      });
+  });
+
+  it('[DELETE /api/v1/grupo-usuario/:id/permissao/:permissaoId] Adicionar um usuário no grupo', async () => {
+    await request(app.getHttpServer())
+      .delete('/api/v1/grupo-usuario/' + paramsGrupo['id'] + '/permissao/' + permissao.id)
+      .set('Authorization', 'Bearer ' + access_token)
+      .expect(HttpStatus.OK)
+      .then((retorno) => {
+        expect(retorno.body.mensagem).toContain('Permissão removida do grupo!');
       });
   });
 
